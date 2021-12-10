cf4a7e3b4172ac64d266a994deeb675073517975
ahmadm712 <ahmadmuji2808@gmail.com>
2021-12-11 01:04:10 +0700

    update tugas

---
+++ 27-RESTful-API-Express-Sequelize/models/tbl_users.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class tbl_users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {}
+  }
+  tbl_users.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "tbl_users",
+    }
+  );
+  return tbl_users;
+};
---
+++ 28-Testing-API-and-Documentation/README.md
@@ -0,0 +1,27 @@
+## Get All Todo List
+
+![Get All Data](get_all_todo.png)
+
+## Get by Todo ID
+
+![Get Data by ID](get_todo_by_id.png)
+
+## Post New Todo
+
+![Create new data](post_new_todo.png)
+
+## Update Todo by id
+
+![Update data](update_todo.png)
+
+## Delete by id
+
+![Delete data](delete_todo.png)
+
+## Documentation
+
+https://documenter.getpostman.com/view/14842444/UVR4PA91
+
+<br>
+
+![Documentation](DOC.png)
